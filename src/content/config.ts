223f15bee8b935e05839348ecad76669dee1d88e
QuantiniumX <vaibhavvx331@gmail.com>
2024-09-03 11:39:06 +0530

    made the base template of everything. just gotta add stuff to it now

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,14 +2,10 @@ import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
 	type: 'content',
-	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
 	}),
 });
 
@@ -22,4 +18,12 @@ const daily = defineCollection({
 	}),
 });
 
-export const collections = { blog, daily };
+const resource = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+	})
+})
+
+export const collections = { blog, daily, resource};
---
+++ src/content/resource/computer_science.md
@@ -0,0 +1,5 @@
+---
+title: 'Computer Science'
+description: 'Lorem ipsum dolor sit amet'
+---
+

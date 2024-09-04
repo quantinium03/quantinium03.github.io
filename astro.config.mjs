d32ea509dfcaf2698e3d1b91a718bebb03231dec
QuantiniumX <vaibhavvx331@gmail.com>
2024-09-05 02:37:10 +0530

    fixed the size of text

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import { VitePlugin } from 'vite-plugin-vue2';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
@@ -8,12 +7,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		build: {
-			minify: 'esbuild',
-		},
-		plugins: [VitePlugin()]
-	},
 	site: 'https://quantiniumx.github.io/',
 	integrations: [mdx(), sitemap(), tailwind()],
 });

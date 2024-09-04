9ec5617245549d1a47b585e343e8cc510b0acbc4
QuantiniumX <vaibhavvx331@gmail.com>
2024-09-05 02:07:26 +0530

    favicon is broken and converted the jpeg to avif

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://quantinium03.github.io/',
+    site: 'https://quantiniumx.github.io/',
     integrations: [mdx(), sitemap(), tailwind()],
 });

1b97fe99481ee3813972a72b920d2fb2cb1ca5a7
QuantiniumX <vaibhavvx331@gmail.com>
2024-09-05 02:17:41 +0530

    minimize js ?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import { VitePlugin } from 'vite-plugin-vue2';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
@@ -7,6 +8,12 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://quantiniumx.github.io/',
-    integrations: [mdx(), sitemap(), tailwind()],
+	vite: {
+		build: {
+			minify: 'esbuild',
+		},
+		plugins: [VitePlugin()]
+	},
+	site: 'https://quantiniumx.github.io/',
+	integrations: [mdx(), sitemap(), tailwind()],
 });
